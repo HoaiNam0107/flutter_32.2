fadbe1d7b7a4767d0747995500840713e45a3331
atoa <namnguyen24052001@gmail.com>
2025-06-07 08:43:39 +0700

    fix: icon

--- lib/main.dart
+++ lib/main.dart
@@ -22,7 +22,7 @@ class MyApp extends StatelessWidget {
       debugShowCheckedModeBanner: false,
       title: 'Custom Snackbar Demo',
       theme: ThemeData(primarySwatch: Colors.blue, useMaterial3: true),
-      home: const HomeTiktokScreen(),
+      home: const TinderSwipeCardScreen(),
     );
   }
 }
